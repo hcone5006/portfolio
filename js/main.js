4fa68ef97b43fbb202d749900329498d1918a3e1
hcone5006 <heidicone@gmail.com>
2014-10-31 17:24:46 +1300

    scroll function
    scroll function and social media icons added

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
 		//initiate MixItUp
 		$('#all-work').mixItUp();
 	});
+	$("li a").click(function(){
+		$("html, body").animate({
+			scrollTop: $("#about").offset().top
+		}, 2000);
+	});
+	$("footer a").click(function(){
+		$("html, body").animate({
+			scrollTop: $("#home").offset().top
+		}, 2000);
+	});
 })
